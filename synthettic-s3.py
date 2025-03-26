036bffeb343aac0b71d93bb80873451dc98eaf3b
Yehuda Sadeh <ysadehwe@ibm.com>
2025-03-26 12:10:46 -0400

    sync

--- synthettic-s3.py
+++ synthettic-s3.py
@@ -3,12 +3,13 @@ import boto3
 import hashlib
 import string
 import json
-from datetime import datetime, UTC
+import time
 import argparse
+import random
+
+from random import choices, randrange, sample
+from datetime import datetime, UTC
 from colorama import Fore, Style, init as colorama_init
-
-
-from random import choices, randrange
 
 class WorkloadConfig:
     def __init__(self):
@@ -47,20 +48,26 @@ def create_snapshot(env, snap_name, description):
     #           'Description': snap['Info']['Description'] }
     #print(json.dumps(result, indent=4))
 
-def list_snapshots(env):
+def list_snapshots(env, start_after = None):
     enable_snapshots(env)
     response = env.s3.list_bucket_snapshots(Bucket = env.conf.bucket_name)
     snaps = response['Snapshots']
 
+    ret_snaps = []
+
     snaps_by_name = {}
 
-    for k in snaps:
-        snap_id = k['ID']
-        snap_name = k['Info']['Name']
+    for snap in snaps:
+        snap_id = snap['ID']
+        if start_after is not None and int(snap_id) <= int(start_after):
+            continue
+
+        ret_snaps.append(snap)
+        snap_name = snap['Info']['Name']
 
         snaps_by_name[snap_name] = snap_id
 
-    return snaps, snaps_by_name
+    return ret_snaps, snaps_by_name
 
 def get_snap_id(env, snap_id, snap_name):
     if snap_id is not None:
@@ -76,11 +83,13 @@ def get_snap_id(env, snap_id, snap_name):
 
     return snap_id
 
-def list_objects(env):
-    if env.snap_range is None:
+def list_objects(env, snap_range = None):
+    eff_snap_range = snap_range or env.snap_range
+    if eff_snap_range is None:
         return env.s3.list_objects(Bucket=env.conf.bucket_name)['Contents']
     else:
-        return env.s3.list_objects(Bucket=env.conf.bucket_name, SnapRange = env.snap_range)['Contents']
+        result = env.s3.list_objects(Bucket=env.conf.bucket_name, SnapRange = eff_snap_range)
+        return result.get('Contents', [])
 
 def get_object_key(env, obj_id):
     return f"{env.conf.prefix}object_{obj_id}.txt"
@@ -96,11 +105,12 @@ def load_metadata(env):
     return { 'objects': {},
             'generated_at': None }
 
-def get_object(env, bucket_name, key):
-    if env.snap_id is None:
+def get_object(env, bucket_name, key, snap_id = None):
+    eff_snap_id = snap_id or env.snap_id
+    if eff_snap_id is None:
         response = env.s3.get_object(Bucket=bucket_name, Key=key)
     else:
-        response = env.s3.get_object(Bucket=bucket_name, Key=key, SnapId=int(env.snap_id))
+        response = env.s3.get_object(Bucket=bucket_name, Key=key, SnapId=int(eff_snap_id))
     return response['Body'].read().decode('utf-8')
 
 def put_object(env, bucket_name, key, data):
@@ -112,17 +122,24 @@ class Env:
         self.s3 = boto3.client("s3")
 
         self.snap_name = conf.snap_name
+        self.from_snap_name = conf.from_snap_name
         if init_snapshot:
             self.snap_id = get_snap_id(self, conf.snap_id, conf.snap_name)
+            self.from_snap_id = get_snap_id(self, conf.from_snap_id, conf.from_snap_name)
         else:
             self.snap_id = conf.snap_id
+            self.from_snap_id = conf.from_snap_id
         
         self.snap_range = None
         if self.snap_id is not None:
             self.snap_range = str(self.snap_id)
 
+        snap_id_str = self.snap_range or ''
+
         if conf.all_objs:
-            self.snap_range = '-' + self.snap_range
+            self.snap_range = f"-{snap_id_str}"
+        elif conf.from_snap_id is not None:
+            self.snap_range = f"{conf.from_snap_id}-{snap_id_str}"
 
 
 class SyntheticS3Workload:
@@ -132,13 +149,12 @@ class SyntheticS3Workload:
         env.s3.create_bucket(Bucket=self.conf.bucket_name)
         self.metadata = load_metadata(env)
 
-    def gen_object(self):
+    def gen_object(self, obj_id):
         object_size = randrange(self.conf.obj_max_size)
         content = random_text(object_size)
         content_bytes = content.encode('utf-8')
         hash_digest = sha256(content_bytes)
-        obj_num = randrange(self.conf.max_obj_ids)
-        object_key = get_object_key(self.env, obj_num + 1)
+        object_key = get_object_key(self.env, obj_id)
 
         put_object(self.env, self.conf.bucket_name, object_key, content_bytes)
 
@@ -150,15 +166,18 @@ class SyntheticS3Workload:
             "sha256": hash_digest
         }
 
-        return obj_num + 1, obj_meta
+        return obj_meta
 
 
     def generate_objects(self):
         num_objs = randrange(self.conf.max_objs) + 1
-        for i in range(num_objs):
-            obj_num, obj_meta = self.gen_object()
+        obj_ids = random.sample(range(1, 100), num_objs)
+        for obj_id in sorted(obj_ids):
+            obj_meta = self.gen_object(obj_id)
 
-            self.metadata['objects'][str(obj_num)] = obj_meta
+            self.metadata['objects'][str(obj_id)] = obj_meta
+
+        print(f'Created {num_objs} objects')
 
 
     def flush_meta(self):
@@ -177,8 +196,6 @@ class SyntheticS3Workload:
 
         success = True
         fail_count = 0
-
-        print(self.metadata)
 
         for obj_id, obj_info in dict(sorted(self.metadata['objects'].items(), key=lambda item: int(item[0]))).items():
             key = obj_info['object_key']
@@ -206,16 +223,46 @@ class SyntheticS3Workload:
             print(f'{fail_count} objects with unexpected content')
             print(Fore.RED + 'FAIL')
 
-    def copy_bucket(self, dest_bucket):
+    def copy_bucket(self, dest_bucket, snap_id = None, incremental = False):
         self.env.s3.create_bucket(Bucket=dest_bucket)
-        for key in list_objects(self.env):
+        snap_range = None
+        if snap_id:
+            snap_range = str(snap_id)
+
+            if not incremental:
+                snap_range = '-' + snap_range
+
+        for key in list_objects(self.env, snap_range = snap_range):
             k = key['Key']
             if not k.startswith(self.conf.prefix):
                 continue
             print(f'copying {self.conf.bucket_name}/{k} -> {dest_bucket}/{k}')
 
-            obj_data = get_object(self.env, self.conf.bucket_name, k)
+            obj_data = get_object(self.env, self.conf.bucket_name, k, snap_id)
             put_object(self.env, dest_bucket, k, obj_data)
+
+    def sync_bucket(self, dest_bucket, follow_snaps, cur_snap_id, incremental):
+        if not follow_snaps:
+            self.copy_bucket(dest_bucket)
+
+        else:
+            snaps, _ = list_snapshots(self.env, cur_snap_id)
+
+            if len(snaps) > 0 and not incremental:
+                snaps = [ snaps[-1] ] # the last snapshot
+
+            for snap in snaps:
+                cur_snap_id=snap['ID']
+                cur_snap_name=snap['Info']['Name']
+
+                inc_str = 'incremental' if incremental else 'full'
+                print(f'Syncing snapshot: {cur_snap_name} ({inc_str} sync)')
+
+                self.copy_bucket(dest_bucket, snap_id = cur_snap_id, incremental=incremental)
+
+                print(f'Finished syncing snapshot: {cur_snap_name} ({inc_str} sync)')
+
+        return cur_snap_id
 
 
 def main():
@@ -225,7 +272,8 @@ def main():
     parser = argparse.ArgumentParser(description="Script that takes a command and an optional bucket.")
     
     # Positional argument
-    parser.add_argument('command', choices=['generate', 'create-snapshot', 'get-meta', 'validate', 'list-snapshots', 'copy-bucket', 'list-objects'], type=str)
+    parser.add_argument('command', choices=['generate', 'create-snapshot', 'get-meta', 'validate', 'list-snapshots',
+                                            'copy-bucket', 'sync-bucket', 'list-objects'], type=str)
     
     # Optional argument
     parser.add_argument('--bucket', '-b', type=str, required=True)
@@ -234,7 +282,13 @@ def main():
     parser.add_argument('--snap-id', type=str)
     parser.add_argument('--description', type=str)
     parser.add_argument('--dest-bucket', type=str)
+    parser.add_argument('--from-snap-id', type=str)
+    parser.add_argument('--from-snap-name', type=str)
     parser.add_argument('--all-objs', action='store_true')
+    parser.add_argument('--forever', action='store_true')
+    parser.add_argument('--follow-snapshots', action='store_true')
+    parser.add_argument('--auto-snap', action='store_true')
+    parser.add_argument('--auto-snap-ratio', type=int, default=5)
 
     args = parser.parse_args()
 
@@ -245,6 +299,8 @@ def main():
     conf.prefix = args.prefix or conf.prefix
     conf.snap_id = args.snap_id
     conf.snap_name = args.snap_name
+    conf.from_snap_id = args.from_snap_id
+    conf.from_snap_name = args.from_snap_name
     conf.all_objs = args.all_objs
 
     init_snapshot = args.command != 'create-snapshot'
@@ -252,8 +308,24 @@ def main():
     workload = SyntheticS3Workload(env)
 
     if args.command == 'generate':
-        workload.generate_objects()
-        workload.flush_meta()
+        snap_ratio = args.auto_snap_ratio or 5
+        i = 0
+        while True:
+            workload.generate_objects()
+            workload.flush_meta()
+
+            if args.auto_snap or not args.forever:
+                i += 1
+                if i % snap_ratio:
+                    snap_name='snap-' + random_text(8)
+
+                    create_snapshot(env, snap_name, 'Auto snapshot')
+
+            if not args.forever:
+                break
+
+            time.sleep(2)
+
     elif args.command == 'create-snapshot':
         if not args.snap_name:
             parser.error("--snap-name is required")
@@ -289,6 +361,22 @@ def main():
             parser.error("--dest-bucket is required")
 
         workload.copy_bucket(args.dest_bucket)
+
+    elif args.command == 'sync-bucket':
+        if not args.dest_bucket:
+            parser.error("--dest-bucket is required")
+
+        cur_snap_id = None
+        incremental = False
+        while True:
+            cur_snap_id = workload.sync_bucket(args.dest_bucket, args.follow_snapshots, cur_snap_id, incremental)
+            if not args.forever:
+                break
+
+            incremental = True
+
+            time.sleep(2)
+
 
 if __name__ == "__main__":
     main()
